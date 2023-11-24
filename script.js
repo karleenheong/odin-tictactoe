1f0ed2da7c787c4666272e48704b0b2992e7fd23
Karleen Heong <17855516+karleenheong@users.noreply.github.com>
2023-11-24 20:53:07 +1100

    Create game logic and place computer choices

--- script.js
+++ script.js
@@ -1,69 +1,113 @@
 const gameboard = (function(){
-  let board = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-  ];
+  let board = ["", "", "", "", "", "", "", "", ""];
 
   const getBoard = () => board;
 
   const displayBoard = () => {
-    for(let i=0; i<board.length; i++){
-      for(let j=0; j<board[i].length; j++){
-        console.log(board[i][j]);
-      }
-    };
+    console.log(board);
+    // for(let i=0; i<board.length; i++){
+    //   console.log(board[i]);
+    // };
   };
 
   const updateBoard = (player, position) => {
-    for(let i=0; i<board.length; i++){
-      for(let j=0; j<board[i].length; j++){
-        if(board[i][j] == position){
-          board[i][j] = player.getSymbol();
-        }
-      }
-    };
+    board[position] = player.getSymbol();
   }
 
   return {displayBoard, updateBoard, getBoard};
 })();
 
 function player(sym){
-let score = 0;
 const symbol = sym;
 
 const getSymbol = () => symbol;
 
-const getScore = () => score;
-
-return {getScore, getSymbol};
+return {getSymbol};
 };
 
 
 const gameController = (function(){
-  let gameOver = false;
+  let playerWins = false;
+  let computerWins = false;
+  let gameInPlay = true;
   const human = player("X");
   const computer = player("O");
 
-  let playerInput = null;
 
   const playGame = () => {
-    //process player input
-    takeInput();
-    gameboard.updateBoard(human, playerInput);
+
+  // gameloop:
+  // while(gameInPlay){
+    
+  // }
+  playerTurn();
+  computerTurn();
+  playerTurn();
+  computerTurn();
+  // 
+    // 
+      
+    //   playerTurn();
+    //   playerWins = checkForWin(human.getSymbol());
+    //   if(playerWins){
+    //     gameInPlay = false;
+    //     console.log("Game over. You win!");
+    //     break gameloop;
+    //   };
+    //   computerTurn();
+    //   computerWins = checkForWin(computer.getSymbol());
+    //   if(computerWins){
+    //     gameInPlay = false;
+    //     console.log("Game over. Computer wins.");
+    //     break gameloop;
+    //   }
+
+  };
+
+  //process player
+  const playerTurn = () => {
+    let playerInput = null;
+    playerInput = prompt("Select your position: ");
+    gameboard.updateBoard(human, +playerInput);
     gameboard.displayBoard();
   };
 
-  // while(!gameOver){
-
-  // };
-  const takeInput = () => {
-    playerInput = prompt("Select your position: ");
+  //check square for winner
+  const checkForWin = (symbol) => {
+    let won = false;
+    let board = gameboard.getBoard();
+    if(
+      (board[0] === symbol && board[1] === symbol && board[2] === symbol) ||
+      (board[0] === symbol && board[4] === symbol && board[8] === symbol) ||
+      (board[0] === symbol && board[3] === symbol && board[6] === symbol) ||
+      (board[1] === symbol && board[4] === symbol && board[7] === symbol) ||
+      (board[2] === symbol && board[5] === symbol && board[8] === symbol) ||
+      (board[2] === symbol && board[4] === symbol && board[6] === symbol) ||
+      (board[3] === symbol && board[4] === symbol && board[5] === symbol) ||
+      (board[6] === symbol && board[7] === symbol && board[8] === symbol)
+      ){
+        won = true;
+      }
+      return won;
   };
 
- 
+  //process computer
+  const computerTurn = () => {
+    let board = gameboard.getBoard();
+    let availablePositions = [];
+    for(let i=0; i<board.length; i++){
+      if(board[i] === ""){
+        availablePositions.push(i);
+      }
+    };
+    let randomNum = Math.floor(Math.random() * availablePositions.length);
+    gameboard.updateBoard(gameController.getComputer(), availablePositions[randomNum]);
+    gameboard.displayBoard();
+  }
 
-  return {playGame};
+  const getComputer = () => computer;
+
+  return {playGame, getComputer};
 })();
 
 
