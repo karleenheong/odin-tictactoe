30f15f5fefd83a72e17252d34a20f97d83c6480a
Karleen Heong <17855516+karleenheong@users.noreply.github.com>
2023-11-26 01:30:58 +1100

    Refactor code for human players
    Create player types screen and name input screen. Game is currently broken.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//---------- GAME BOARD DATA -------------
+
 const gameboard = (function(){
   let board = ["", "", "", "", "", "", "", "", ""];
 
@@ -15,26 +17,58 @@ const gameboard = (function(){
   return {updateBoard, getBoard, resetBoard};
 })();
 
-function player(sym){
-  const symbol = sym;
-  let name;
-  let score = 0;
 
-  const getSymbol = () => symbol;
-  const getScore = () => score;
-  const addToScore = () => score++;
+//---------- PLAYER FACTORY -------------
+
+const player = () => {
+  let name;
+  let type;
+  let symbol = null;
+  let icon = null;
+  let background = null;
+  let score = 0;
 
   const getName = () => name;
   const setName = (playerName) => name = playerName;
+  const getType = () => type;
+  const setType = (playerType) => type = playerType;
+  const getSymbol = () => symbol;
+  const setSymbol = (sym) => symbol = sym;
+  const getIcon = () => icon;
+  const setIcon = (pic) => icon = pic;
+  const getBG = () => background;
+  const setBG = (pic) => background = pic;
+  const getScore = () => score;
+  const addToScore = () => score++;
 
-  return {getSymbol, getName, setName, getScore, addToScore};
-};
+  return {getType, setType, getName, setName, getSymbol, setSymbol, getIcon, setIcon, getBG, setBG, getScore, addToScore};
+}
 
+
+//---------- GAME CONTROLLER -------------
 
 const gameController = (function(){
-  let playerOne = null;
-  let playerTwo = null;
+  let players = [];
   let rounds = 0;
+
+  players[0] = player();
+  players[1] = player();
+
+  const determinePlayers = (button) => {
+    if(button.id === "humanVsHuman"){
+      players[0].setType("human");
+      players[1].setType("human");
+    } else if(button.id === "humanVsComp"){
+      players[0].setType("human");
+      players[1].setType("computer");
+    } else if(button.id === "compVsComp"){
+      players[0].setType("computer");
+      players[1].setType("computer");
+    } else {
+      console.log("player select error");
+    }
+    displayController.processPlayerNames();
+  }
 
   const assignCross = () => {
     playerOne = player("X");
@@ -52,52 +86,70 @@ const gameController = (function(){
     displayController.showGameboard();
   }
 
-  //process player
-  const playerOneTurn = (square) => {
+  //Process Human Player
+  const humanTurn = (square) => {
 
     switch(square.id){
       case "btn0":
         gameboard.updateBoard(playerOne, 0);
-        displayController.updateButton(playerOne, 0);
+        displayController.updateSquare(playerOne, 0);
         break;
       case "btn1":
         gameboard.updateBoard(playerOne, 1);
-        displayController.updateButton(playerOne, 1);
+        displayController.updateSquare(playerOne, 1);
         break;
       case "btn2":
         gameboard.updateBoard(playerOne, 2);
-        displayController.updateButton(playerOne, 2);
+        displayController.updateSquare(playerOne, 2);
         break;
       case "btn3":
         gameboard.updateBoard(playerOne, 3);
-        displayController.updateButton(playerOne, 3);
+        displayController.updateSquare(playerOne, 3);
         break;
       case "btn4":
         gameboard.updateBoard(playerOne, 4);
-        displayController.updateButton(playerOne, 4);
+        displayController.updateSquare(playerOne, 4);
         break;
       case "btn5":
         gameboard.updateBoard(playerOne, 5);
-        displayController.updateButton(playerOne, 5);
+        displayController.updateSquare(playerOne, 5);
         break;
       case "btn6":
         gameboard.updateBoard(playerOne, 6);
-        displayController.updateButton(playerOne, 6);
+        displayController.updateSquare(playerOne, 6);
         break;
       case "btn7":
         gameboard.updateBoard(playerOne, 7);
-        displayController.updateButton(playerOne, 7);
+        displayController.updateSquare(playerOne, 7);
         break;
       case "btn8":
         gameboard.updateBoard(playerOne, 8);
-        displayController.updateButton(playerOne, 8);
+        displayController.updateSquare(playerOne, 8);
         break;
       default:
         console.log("error on player turn");
     }
   };
 
-  //check square for winner
+  //Process Computer Player
+  const computerTurn = () => {
+    let board = gameboard.getBoard();
+    let availablePositions = [];
+    for(let i=0; i<board.length; i++){
+      if(board[i] === ""){
+        availablePositions.push(i);
+      }
+    }
+    if(availablePositions.length > 0){
+      let randomNum = Math.floor(Math.random() * availablePositions.length);
+      gameboard.updateBoard(gameController.getPlayerTwo(), availablePositions[randomNum]);
+      displayController.updateSquare("playerTwo", availablePositions[randomNum]);
+    } else {
+      displayController.displayGameEnd("noone");
+    }
+  }
+
+  //Check Square for Winner
   const checkForWin = (player) => {
     let won = false;
     let board = gameboard.getBoard();
@@ -121,7 +173,7 @@ const gameController = (function(){
           displayController.displayGameEnd(player);
         } else {
           if(player === playerOne){
-            playerTwoTurn();
+            computerTurn();
           } 
         }
     } else {
@@ -134,6 +186,7 @@ const gameController = (function(){
     player.addToScore();
   }
 
+  //Check Gameboard Still Has Space
   const checkGameEnded = () => {
     let gameEnded;
     let board = gameboard.getBoard();
@@ -147,33 +200,19 @@ const gameController = (function(){
     return gameEnded;
   }
 
-  //process playerTwo
-  const playerTwoTurn = () => {
-    let board = gameboard.getBoard();
-    let availablePositions = [];
-    for(let i=0; i<board.length; i++){
-      if(board[i] === ""){
-        availablePositions.push(i);
-      }
-    }
-    if(availablePositions.length > 0){
-      let randomNum = Math.floor(Math.random() * availablePositions.length);
-      gameboard.updateBoard(gameController.getPlayerTwo(), availablePositions[randomNum]);
-      displayController.updateButton("playerTwo", availablePositions[randomNum]);
-    } else {
-      displayController.displayGameEnd("noone");
-    }
-  }
-
-  const getPlayerTwo = () => playerTwo;
-  const getPlayerOne = () => playerOne;
+  const getPlayers = () => players;
+  // const getPlayerTwo = () => playerTwo;
+  // const getPlayerOne = () => playerOne;
   const getRounds = () => rounds;
 
-  return {getPlayerTwo, getPlayerOne, assignCross, assignCircle, playerOneTurn, checkForWin, playerTwoTurn, getRounds};
+  return {determinePlayers, getPlayers, assignCross, assignCircle, humanTurn, checkForWin, computerTurn, getRounds};
 })();
 
-//dom controller
+
+//---------- DISPLAY CONTROLLER -------------
+
 const displayController = (function() {
+
   const selectWeapon = document.querySelector(".selectWeapon");
   const gameScreen = document.querySelector(".gameScreen");
   const restart = document.querySelector(".restart");
@@ -182,48 +221,78 @@ const displayController = (function() {
   const scorePanel = document.querySelector(".scorePanel");
   const playerOneScore = document.querySelector(".playerOneScore");
   const playerTwoScore = document.querySelector(".playerTwoScore");
+  const playerSelect = document.querySelector(".playerSelect");
+  const nameScreen = document.querySelector(".nameScreen");
+  const weaponText = document.querySelector(".weaponText");
+
+  const playerSelectBtns = document.querySelectorAll(".playerSelectBtn");
+  for(let i=0; i<playerSelectBtns.length; i++){
+    playerSelectBtns[i].addEventListener("click", function(){
+      gameController.determinePlayers(this);
+    });
+  }
+
+  const crossBtn = document.querySelector(".crossBtn");
+  const circleBtn = document.querySelector(".circleBtn");
+  crossBtn.addEventListener("click", gameController.assignCross);
+  circleBtn.addEventListener("click", gameController.assignCircle);
+
+  const squareBtns = document.querySelectorAll(".square");
+  for(let i=0; i<squareBtns.length; i++){
+    squareBtns[i].addEventListener("click", function(){
+      gameController.humanTurn(this);
+    });
+  };
 
   let playerOneIcon;
   let playerTwoIcon;
   let playerOneBG;
   let playerTwoBG;
 
-  const restartGame = () => {
-    console.log(gameController.getRounds());
-    hideRestart();
-    //destroy resultsDiv text
-    while(resultsDiv.firstChild){
-      resultsDiv.removeChild(resultsDiv.firstChild);
-    }
-    gameboard.resetBoard();
-    resetSquares();
-    if(gameController.getRounds() % 2 !== 0){
-      gameController.playerTwoTurn();
-    }
-  }
-  
-  const crossBtn = document.querySelector(".crossBtn");
-  const circleBtn = document.querySelector(".circleBtn");
-  crossBtn.addEventListener("click", gameController.assignCross);
-  circleBtn.addEventListener("click", gameController.assignCircle);
+  //Player Names Screen
+  const processPlayerNames = () => {
+    hidePlayerSelect();
+    showNameScreen();
+    let players = gameController.getPlayers();
 
-  squareBtns = document.querySelectorAll(".square");
-  for(let i=0; i<squareBtns.length; i++){
-    squareBtns[i].addEventListener("click", function(){
-      gameController.playerOneTurn(this);
+    for(let i=0; i<players.length; i++){
+      if(players[i].getType() === "human"){
+        let instructionText = document.createElement("h3");
+        instructionText.textContent = "Player " + (i+1) + ":";
+        nameScreen.appendChild(instructionText);
+        let inputBox = document.createElement("input");
+        inputBox.type = "text";
+        inputBox.className = "inputBox";
+        nameScreen.appendChild(inputBox);
+      }
+    }
+    let newBtn = document.createElement("button");
+    newBtn.className = "submitName";
+    newBtn.textContent = "Let's Begin!";
+
+    newBtn.addEventListener("click", () => {
+      let inputBoxes = document.querySelectorAll(".inputBox");
+      for(let i=0; i<inputBoxes.length; i++){
+        players[i].setName(inputBoxes[i].value);
+        console.log(players[i].getName());
+      }
+      processWeaponSelect(players);
     });
-  };
-
-  const resetSquares = () => {
-    for(let i=0; i<squareBtns.length; i++){
-      squareBtns[i].style.backgroundImage = "url(./graphics/tile.png)";
-      squareBtns[i].style.backgroundSize = "100% 100%"; 
-      squareBtns[i].style.opacity = "50%";
-      squareBtns[i].disabled = false;
-    };
+    
+    nameScreen.appendChild(newBtn);
   }
 
-  restartBtn.addEventListener("click", restartGame);
+  //Weapon Select Screen
+  const processWeaponSelect = (players) => {
+    hideNameScreen();
+    showSelectWeapon();
+
+    let nameText = document.createElement("h2");
+    nameText.textContent = "Choose your weapon, " + players[0].getName() + ":";
+    nameText.style.padding = "30px 0";
+    weaponText.appendChild(nameText);
+  }
+
 
   const setIcons = (icon) => {
     if(icon === "cross"){
@@ -239,7 +308,7 @@ const displayController = (function() {
     }
   }
 
-  const updateButton = (player, num) => {
+  const updateSquare = (player, num) => {
     if(player === gameController.getPlayerOne()) {
       squareBtns[num].style.backgroundImage = playerOneIcon;
       squareBtns[num].style.backgroundColor = playerOneBG;
@@ -273,6 +342,33 @@ const displayController = (function() {
     playerTwoScore.textContent = gameController.getPlayerTwo().getScore();
   }
 
+
+  const restartGame = () => {
+    console.log(gameController.getRounds());
+    hideRestart();
+    //destroy resultsDiv text
+    while(resultsDiv.firstChild){
+      resultsDiv.removeChild(resultsDiv.firstChild);
+    }
+    gameboard.resetBoard();
+    resetSquares();
+    if(gameController.getRounds() % 2 !== 0){
+      gameController.computerTurn();
+    }
+  }
+
+  restartBtn.addEventListener("click", restartGame);
+  
+  const resetSquares = () => {
+    for(let i=0; i<squareBtns.length; i++){
+      squareBtns[i].style.backgroundImage = "url(./graphics/tile.png)";
+      squareBtns[i].style.backgroundSize = "100% 100%"; 
+      squareBtns[i].style.opacity = "50%";
+      squareBtns[i].disabled = false;
+    };
+  }
+
+
   const showRestart = () => {
     restart.style.display = "block";
   }
@@ -297,17 +393,28 @@ const displayController = (function() {
   const hideScore = () => {
     scorePanel.style.display = "none";
   }
+  const showPlayerSelect = () => {
+    playerSelect.style.display = "block";
+  }
+  const hidePlayerSelect = () => {
+    playerSelect.style.display = "none";
+  }
+  const showNameScreen = () => {
+    nameScreen.style.display = "block";
+  }
+  const hideNameScreen = () => {
+    nameScreen.style.display = "none";
+  }
 
+  hideNameScreen();
+  hideSelectWeapon();
   hideGameboard();
   hideRestart();
- // hideScore();
+  hideScore();
 
-  return {hideSelectWeapon, showGameboard, hideGameboard, updateButton, setIcons, displayGameEnd};
+  return {processPlayerNames, hideSelectWeapon, showGameboard, hideGameboard, updateSquare, setIcons, displayGameEnd};
 })();
 
-
-//main
-// gameController.playGame();
 
 
 
